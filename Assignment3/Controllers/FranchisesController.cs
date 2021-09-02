963e7dffb27c685f738eefaf2eada6769da6899b
sebastian.soderholm <sebastian.soderholm@fi.experis.com>
2021-09-02 21:42:07 +0300

    Added documentation.

--- Assignment3/Controllers/FranchisesController.cs
+++ Assignment3/Controllers/FranchisesController.cs
@@ -4,6 +4,7 @@ using Assignment3.Models.DTO.Character;
 using Assignment3.Models.DTO.Franchise;
 using Assignment3.Models.DTO.Movie;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -30,7 +31,10 @@ namespace Assignment3.Controllers
             Context = context;
             Mapper = mapper;
         }
-
+        /// <summary>
+        /// Get all franchises in database
+        /// </summary>
+        /// <returns>Collection of all Franchise objects in database</returns>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<FranchiseReadDTO>>> GetFranchises()
         {
@@ -42,6 +46,7 @@ namespace Assignment3.Controllers
         /// </summary>
         /// <param name="id">Franchise's id value as int</param>
         /// <returns>Franchise object</returns>
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("{id}")]
         public async Task<ActionResult<FranchiseReadDTO>> GetFranchise(int id)
         {
@@ -57,9 +62,13 @@ namespace Assignment3.Controllers
         /// <summary>
         /// Updates a franchise in database.
         /// </summary>
-        /// <param name="id"></param>
-        /// <param name="franchise"></param>
-        /// <returns></returns>
+        /// <param name="id">Id value of franchise to update</param>
+        /// <param name="franchise">Franchise object with updated information</param>
+        /// <returns>No content</returns>
+        /// <Exception>DbUpdateConcurrencyException</Exception>
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPut("{id}")]
         public async Task<IActionResult> PutFranchise(int id, FranchiseEditDTO franchise)
         {
@@ -85,8 +94,8 @@ namespace Assignment3.Controllers
         /// <summary>
         /// Adds a franchise to database.
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="dtoFranchise">Franchise object to add to database</param>
+        /// <returns>Newly created Franchise object</returns>
         [HttpPost]
         public async Task<ActionResult<FranchiseCreateDTO>> PostFranchise(FranchiseCreateDTO dtoFranchise)
         {
@@ -103,7 +112,9 @@ namespace Assignment3.Controllers
         /// Deletes a franchise.
         /// </summary>
         /// <param name="id"></param>
-        /// <returns></returns>
+        /// <returns>No content</returns>
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteFranchise(int id)
         {
@@ -121,7 +132,8 @@ namespace Assignment3.Controllers
         /// Gets all movies for given Franchise ID
         /// </summary>
         /// <param name="id">Franchise's id value as int</param>
-        /// <returns></returns>
+        /// <returns>Collection of movie objects in given franchise</returns>
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("{id}/movies")]
         public async Task<ActionResult<IEnumerable<MovieReadDTO>>> GetFranchiseMovies(int id)
         {
@@ -142,7 +154,10 @@ namespace Assignment3.Controllers
         /// </summary>
         /// <param name="id">ID of Franchise to update</param>
         /// <param name="movieIds">Array of Movie IDs to update in Franchise</param>
-        /// <returns></returns>
+        /// <returns>Ok</returns>
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPut("{id}/movies")]
         public async Task<IActionResult> PutFranchiseMovies(int id, int[] movieIds)
         {
@@ -186,7 +201,8 @@ namespace Assignment3.Controllers
         /// Gets all characters for given Franchise ID
         /// </summary>
         /// <param name="id">Franchise's id value as int</param>
-        /// <returns></returns>
+        /// <returns>Returns a collection of character objects for given franchise</returns>
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("{id}/characters")]
         public async Task<ActionResult<IEnumerable<CharacterReadDTO>>> GetFranchiseCharacters(int id)
         {
@@ -204,6 +220,11 @@ namespace Assignment3.Controllers
             return Mapper.Map<List<CharacterReadDTO>>(characters);
         }
 
+        /// <summary>
+        /// Helper function to check if given franchise exists in database.
+        /// </summary>
+        /// <param name="id">Franchise's Id value to check</param>
+        /// <returns>True if franchise exists in database, false if not.</returns>
         private bool FranchiseExists(int id)
         {
             return Context.Franchises.Any(e => e.Id == id);
